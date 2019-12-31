672f1df641311116df550fff14772c175e77ae60
r-kimura001 <torakichisite@gmail.com>
2019-12-31 22:56:19 +0900

    feat:Topページのサーフィックスを更新

--- laravel/resources/js/pages/Top.vue
+++ laravel/resources/js/pages/Top.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="p-top">
-    <h1 class="SiteTitle">士業マッチングサイトです:57</h1>
+    <h1 class="SiteTitle">士業マッチングサイトです:58</h1>
     <div>
       <img src="https://asset.shigyo-match.site/assets/0h0IyXKhWjb2ZEIUVE5ZMQMQt8aQk9QnVuLll4XDF3ZUgxTXhmLFt2UD9-eQY2FCgzZDdjSWghUSkfQy9MMRBIcB8lahQdWFAxECFVcDtgdzEyDSg0fU4hBmAhMVFpGXpifkUiRWApOFY8E300.jpeg" alt="">
     </div>
