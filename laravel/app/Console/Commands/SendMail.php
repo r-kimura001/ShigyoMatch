56e2d220e5a1c7c7a90dff388a8683700e411d62
r-kimura001 <torakichisite@gmail.com>
2020-04-12 16:22:30 +0900

    SES利用に必要なcomposerライブラリインストール、メール送信テスト用コマンド作成

---
+++ laravel/app/Console/Commands/SendMail.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\ShigyoMail;
+
+class SendMail extends Command
+{
+  /**
+   * The name and signature of the console command.
+   *
+   * @var string
+   */
+  protected $signature = 'send_mail';
+
+  /**
+   * The console command description.
+   *
+   * @var string
+   */
+  protected $description = 'メールを送信します';
+
+  /**
+   * Create a new command instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+    parent::__construct();
+  }
+
+  /**
+   * Execute the console command.
+   *
+   * @return mixed
+   */
+  public function handle()
+  {
+    Mail::to('rcr126126@gmail.com')->send(new ShigyoMail());
+  }
+}
---
+++ laravel/app/Mail/ShigyoMail.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class ShigyoMail extends Mailable
+{
+  use Queueable, SerializesModels;
+
+  /**
+   * Create a new message instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+    //
+  }
+
+  /**
+   *
+   */
+  public function build()
+  {
+    return $this->from( config('mail.from.address') )->view('mail.example');
+  }
+}
